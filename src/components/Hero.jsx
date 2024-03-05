bb0c7ce8515c99d487404ddd738a31aea1de8c2b
Shrewdkenny <shrewdkenny@gmail.com>
2024-03-05 18:17:17 +0100

    ds

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { FaTimes } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <div>
-      <div className="container  flex justify center lg:flex flex-col justify-center items-center mt-[150px] h-[100%] w-full tracking-wide">
+    <>
+      <div className="container flex justify center lg:flex flex-col justify-center items-center mt-[150px]  w-full tracking-wide">
         <h1 className="lg:flex flex-wrap mt-[150px] tracking-wide text-center text-5xl font-bold  font-sans-serif  text-gray-800 leading-snug">
           Create and Submit Assignments <br /> with Ease
         </h1>
@@ -13,35 +14,29 @@ const Hero = () => {
           <br />
           and for students to view and submit them.
         </p>
-        <button className="flex justify-center items-center lg:border bg-[#18753D] text-white font-semi-normal rounded-full mb-40 px-10 py-4 mt-10">
+        <button className="flex justify-center items-center lg:border bg-[#18753D] text-white font-semi-normal rounded-full mb-20 px-10 py-4 mt-10">
           Request a Demo
         </button>
-        <div className="flex flex-row w-full bg-[#fefefe]">
-          <div className="flex gap-2">
-            <img
-              src="./src/assets/images/logo.png"
-              alt=""
-              className="mt-1 h-20"
-            />
-            <h1 className="mt-4 text-3xl font-medium">Kwarapoly</h1>
+
+        {/* After Demo Button  */}
+
+        <div className="container flex flex-row w-full h-[80vh]  bg-[#fefefe]">
+          <div className="flex flex-col bg-[#ffffff] mt-20 w-[250px] p-5">
+            <div className="flex gap-2">
+              <img src="./src/assets/images/logo.png" alt="" className=" h-7" />
+              <h1 className=" text-xl font-medium">Kwarapoly</h1>
+            </div>
+            <h1 className="mt-20">Menu</h1>
+          </div>
+          <div className="flex flex-row justify-between">
+            <div className="mt-20  ">
+              <input type="text" placeholder="search or type" />
+            </div>
+           
           </div>
         </div>
       </div>
-
-      {/* <div className="flex flex-row   bg-[#fefefe] w-full h-[80vh] py-10">
-        <div className="flex flex-col bg-[#ffffff] w-[400px] h-screen p-10">
-          <div className="flex gap-2 ">
-            <img
-              src="./src/assets/images/logo.png"
-              alt=""
-              className="mt-1 h-20"
-            />
-            <h1 className="mt-4 text-3xl font-medium">Kwarapoly</h1>
-          </div>
-          <h1>Menu</h1>
-        </div>
-      </div> */}
-    </div>
+    </>
   );
 };
 
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,7 +5,7 @@ import Hero from "../components/Hero";
 const HomePage = () => {
   return (
     <>
-      <main className=" flex-wrap lg:flex-nowrap w-full h-screen bg-[#F5F4F6]">
+      <main className=" flex-wrap lg:flex-nowrap w-full h-[300vh] bg-[#F5F4F6]">
         <Header />
         <Hero />
       </main>
