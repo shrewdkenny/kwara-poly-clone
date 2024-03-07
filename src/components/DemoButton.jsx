084a023e5a0b66ad5240e1154bdcbbe498b0d005
Shrewdkenny <shrewdkenny@gmail.com>
2024-03-07 12:21:07 +0100

    dfhhjfd

---
+++ src/components/DemoButton.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DemoButton = () => {
+  return (
+    <div>
+      <button className="flex justify-center items-center lg:border bg-[#18753D] text-white font-semi-normal rounded-full mb-20 px-10 py-4 mt-10">
+        Request a Demo
+      </button>
+    </div>
+  );
+};
+
+export default DemoButton;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,21 @@
 import React from "react";
 import {
+  FaArrowLeft,
+  FaArrowRight,
   FaBell,
   FaBookOpen,
   FaComment,
   FaEnvelope,
   FaHome,
   FaSearch,
+  FaStar,
   FaTools,
 } from "react-icons/fa";
 
 import TopStudents from "./TopStudents";
 import SideBarButtons from "./SideBarButtons";
 import AssignmentsCards from "./AssignmentsCards";
+import DemoButton from "./DemoButton";
 
 const Hero = () => {
   return (
@@ -27,11 +31,96 @@ const Hero = () => {
           <br />
           and for students to view and submit them.
         </p>
-        <button className="flex justify-center items-center lg:border bg-[#18753D] text-white font-semi-normal rounded-full mb-20 px-10 py-4 mt-10">
-          Request a Demo
-        </button>
+        <DemoButton />
+
+        {/* HERO SECTION IMAGE */}
         <div className="flex flex-row justify-center">
           <img src="./src/assets/images/heroimage.png" alt="" />
+        </div>
+
+        {/* TRUSTED BY */}
+        <div className="flex flex-row justify-between mt-[100px]  h-auto w-[78%]">
+          <h1 className="p-2 font-semibold text-xl">Trusted by:</h1>
+          <div className="flex flex-row gap-10">
+            <img src="./src/assets/images/havard.png" alt="" className="h-10" />
+            <img src="./src/assets/images/oxford.png" alt="" className="h-10" />
+            <img
+              src="./src/assets/images/microsoft.png"
+              alt=""
+              className="h-10"
+            />
+            <img src="./src/assets/images/google.png" alt="" className="h-8" />
+          </div>
+        </div>
+
+        {/* FOR STUDENTS */}
+        <div className="flex flex-row gap-40 mt-[150px] h-[418px] w-[95%]">
+          <div className="flex flex-col mt-[100px] w-1/3">
+            <h1 className="text-4xl font-semibold">For Students</h1>
+            <p className="text-sm font-normal leading-5 tracking-wider text-gray-600 font-sans mt-3">
+              View assignments and submit completed work directly through our
+              platform. Track your progress and see how you're doing on each
+              assignment. Accessible from any device, our platform is intuitive
+              and easy to use.
+            </p>
+            <DemoButton />
+          </div>
+          <div>
+            <img src="./src/assets/images/assignments 1.png" alt="" />
+          </div>
+        </div>
+        {/* FOR LECTURER */}
+
+        <div className="flex flex-row gap-40 mt-[150px] h-[418px] w-[95%]">
+          <div>
+            <img src="./src/assets/images/lecturer 1.png" alt="" />
+          </div>
+          <div className="flex flex-col mt-[100px] w-1/3">
+            <h1 className="text-4xl font-semibold">For Lecturer</h1>
+            <p className="text-sm font-normal leading-5 tracking-wider text-gray-600 font-sans mt-3">
+              Create individual or series of assignments, set due dates and
+              grading criteria, and manage submissions with ease. Our
+              user-friendly platform allows you to create assignments with
+              various details such as name, category, level, and lessons.
+            </p>
+            <DemoButton />
+          </div>
+        </div>
+
+        {/* FOOTER */}
+        <div className="h-[500px] bg-[#212121] mt-[150px] w-[100vw] py-20 px-28">
+          <div className="flex flex-row justify-between">
+            <h1 className="text-gray-100 text-2xl font-normal font-sans">
+              TESTIMONIALS
+            </h1>
+            <div className="flex flex-col">
+              <div className="flex flex-row gap-2 text-white text-2xl">
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+              <p className="mt-7 text-[#909090]  leading-7 text-xl font-normal">
+                The Agency Creative team did a brochure for <br /> one of our
+                development projects in Manchester. <br /> The team listened to
+                the brief and came up with <br /> some great ideas, one of which
+                we liked and <br /> picked. They also helped us arrange a
+                videographer <br /> for the show apartment.
+              </p>
+
+              <img
+                src="./src/assets/images/kamaldeen.png"
+                alt=""
+                className="mt-7"
+              />
+              <div className="flex flex-row text-white mt-10 gap-5 ml-4">
+                {" "}
+                <FaArrowLeft />
+                <FaArrowRight />
+              </div>
+            </div>
+          </div>
         </div>
 
         {/* <div className=" flex flex-row w-[100vw] h-screen bg-[#fefefe] px-5">
